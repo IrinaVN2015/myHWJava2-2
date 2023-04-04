8d938fed20214942b3598175e37be4aa12e41496
Irina Vilkova <vilkovairina.80@mail.ru>
2023-04-04 19:45:31 +0300

    Change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 100; //денег на текущем счете у клиента
-        int y = 1500; //сумма пополнения
-        int z = y / 100; //количество бонусных рублей
-        int k = x + y; //сумма денег на счете и суммы пополнения
+        int moneyInAccount = 100; //денег на текущем счете у клиента
+        int deposit = 1500; //сумма пополнения
+        int bonusRubles = deposit / 100; //количество бонусных рублей
+        int allMoney = moneyInAccount + deposit; //сумма денег на счете и суммы пополнения
 
 
-        if (y > 1000) {
-            System.out.println((z) + k + " итоговый счет, включая бонусные рубли ");
-            System.out.println((y / 100 + " количество бонусных рублей "));
+        if (deposit > 1000) {
+            System.out.println((bonusRubles) + allMoney + " итоговый счет, включая бонусные рубли ");
+            System.out.println((deposit / 100 + " количество бонусных рублей "));
         } else {
-            System.out.println(k + " итоговый счет ");
+            System.out.println(allMoney + " итоговый счет ");
        }
 
         }
